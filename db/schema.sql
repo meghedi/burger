43b63cb08bc051a2408af2346bb5e07a3a5a7779
Meghedi Hacopi <hacopian.m@gmail.com>
2020-03-30 13:41:30 -0700

    First commit

---
+++ db/schema.sql
@@ -0,0 +1,14 @@
+### schema
+DROP TABLE burgers_db IF EXISTS;
+
+
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers
+(
+	id int NOT NULL AUTO_INCREMENT,
+	burger_name varchar(255) NOT NULL,
+	devoured BOOLEAN DEFAULT false,
+	PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('Chicken Burger');
+INSERT INTO burgers (burger_name) VALUES ('Beef');
+INSERT INTO burgers (burger_name) VALUES ('Double');
+INSERT INTO burgers (burger_name) VALUES ('Vigan');
+INSERT INTO burgers (burger_name) VALUES ('Veggei');
