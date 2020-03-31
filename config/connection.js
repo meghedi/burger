58baf60aedc2e5f3a04a78ecf5e8354fcc21ef67
Meghedi Hacopi <hacopian.m@gmail.com>
2020-03-31 01:45:41 -0700

    changed port for heroku

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,11 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
+const PORT = process.env.PORT || 3306;
+
 var connection = mysql.createConnection({
   host: "localhost",
-  port: 3306,
+  port: PORT,
   user: "root",
   password: "",
   database: "burgers_db"
