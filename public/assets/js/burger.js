39dd3ca70ae57573e45229c9aa8590fe0270e632
Meghedi Hacopi <hacopian.m@gmail.com>
2020-03-31 01:38:26 -0700

    added functionality

---
+++ public/assets/js/burger.js
@@ -0,0 +1,58 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function() {
+    $(".devouredToggle").on("click", function(event) {
+      var id = $(this).data("id");
+      var thisElem = $(this);
+      
+      var updatedDevoured = {
+        devoured: true
+      };
+  
+      // Send the PUT request.
+      $.ajax("/api/burgers/" + id, {
+        type: "PUT",
+        data: updatedDevoured
+      }).then(
+        function() {
+            thisElem.fadeOut();
+          console.log("changed devoured to", updatedDevoured);
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  
+    $(".create-form").on("submit", function(event) {
+      // Make sure to preventDefault on a submit event.
+      event.preventDefault();
+  
+      var newBurger = {
+        burger_name: $("#burger").val().trim(),
+      };
+  
+      // Send the POST request.
+      $.ajax("/api/burgers", {
+        type: "POST",
+        data: newBurger
+      }).then(
+        function() {
+          console.log("created new Burger");
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  
+     $('.delete-burger').on('click', function(){
+      $.ajax("/api/burgers" + id, {
+        type: "DELETE",
+      }).then(
+        function() {
+          console.log("Burger deleted!");
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+     });
+  });
+  
