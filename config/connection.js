7aa5cf781891ca4af87280f19300b78be5557f1a
Meghedi Hacopi <hacopian.m@gmail.com>
2020-03-31 22:53:02 -0700

    added JAWSDB Setting

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,22 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var connection;
 
 const PORT = process.env.PORT || 3306;
-
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: PORT,
-  user: "root",
-  password: "",
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: PORT,
+    user: "root",
+    password: "",
+    database: "burgers_db"
+  });
+}
 
 // Make connection.
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
